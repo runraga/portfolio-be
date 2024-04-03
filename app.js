08da2a5ff537c4f9925310426c9bbde69a0cacfc
runraga <james@helmfarm.com>
2024-04-03 11:55:20 +0100

    add express error handler

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ app.get("/portfolio-be/public", getPublicRepos);
 
 app.get("/portfolio-be/readme/:repo", getReadmeContents);
 
+app.use((err, req, res, next) => {
+  const { response } = err;
+
+  res.status(response.status).send({ error: response.statusText });
+});
+
 app.listen(8080, (err) => {
   if (err) {
     console.log(err);
