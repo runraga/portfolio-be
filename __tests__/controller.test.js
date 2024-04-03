bc67a10cea2ea44a13250da6a5272d9a693277f9
runraga <james@helmfarm.com>
2024-04-03 11:56:15 +0100

    refactor: add full readme ad key to getReadmeContents object'

--- __tests__/controller.test.js
+++ __tests__/controller.test.js
@@ -12,33 +12,32 @@ describe("convertReadMe", () => {
     expect(result.hasOwnProperty("title")).toBe(true);
     expect(result.title).toBe("Data Lakehouse");
   });
-  test("should return an object with the key 'short description' whose value is the one-line description from the readme", () => {
+  test("should return an object with the key 'description' whose value is the one-line description from the readme", () => {
     const result = convertReadMe(input);
-    expect(result["short description"]).toBe(
+    expect(result["description"]).toBe(
       "An AWS cloud-based solution for processing and transforming online transaction database into an online analytical database lakehouse using an ETL approach."
     );
   });
   test("should return an object with the key 'languagues' whose value is an array of strings detailing the programming languages listed in the readme file", () => {
     const result = convertReadMe(input);
-    const expected = [
-      "Python",
-      "Terraform",
-      "SQL",
-      "GitHub Workflow",
-      "Amazon Web Services",
-    ];
+    const expected =
+      "- Python\n- Terraform\n- SQL\n- GitHub Workflow\n- Amazon Web Services";
     expect(result.languages).toEqual(expected);
   });
   test("should return an object with the key 'overview' whose value is a string describing the project in the readme file", () => {
     const result = convertReadMe(input);
     const expected =
-      "This group project was completed as part of the Northcoders Data Engineering course. It's goal was to create an application to extract, transform and load data from a transactional database to a STAR schema analytics database.\n\nThe minimum viable product produced consisted of: \n- two S3 buckets for extracted and transformed data in parquet format\n- three Python Lambdas to extact data from the OLTP database, transform data in to target STAR schemas and to load data into the OLAP database\n- CloudWatch logs and alarms to monitor lambda performance\n\n[![N|Solid](https://github.com/runraga/nc-de-final-project/blob/main/diagrams/MVP.png)](https://github.com/runraga/nc-de-final-project/blob/main/diagrams/MVP.png)\n\nA CI/CD automation pipeline was set up for the application using:\n- Git Hooks to enforce fully unit tested (Jest), PEP8 compliant and 'safe' python code\n- Terraform to manage infrastructure in AWS\n- GitHub workflow for continuous deployment of code as it was commited to the main branch\n\n";
+      "This group project was completed as part of the Northcoders Data Engineering course. It's goal was to create an application to extract, transform and load data from a transactional database to a STAR schema analytics database.\n\nThe minimum viable product produced consisted of: \n- two S3 buckets for extracted and transformed data in parquet format\n- three Python Lambdas to extact data from the OLTP database, transform data in to target STAR schemas and to load data into the OLAP database\n- CloudWatch logs and alarms to monitor lambda performance\n\n[![N|Solid](https://github.com/runraga/nc-de-final-project/blob/main/diagrams/MVP.png)](https://github.com/runraga/nc-de-final-project/blob/main/diagrams/MVP.png)\n\nA CI/CD automation pipeline was set up for the application using:\n- Git Hooks to enforce fully unit tested (Jest), PEP8 compliant and 'safe' python code\n- Terraform to manage infrastructure in AWS\n- GitHub workflow for continuous deployment of code as it was commited to the main branch";
     expect(result.overview).toEqual(expected);
   });
-  test("should return an object with the key 'future features' whose value is a string describing changes in future version from the readme file", () => {
+  test("should return an object with the key 'future' whose value is a string describing changes in future version from the readme file", () => {
     const result = convertReadMe(input);
     const expected =
-      "- Improvements to integration testing\n- Restructure ingestion bucket data to better track raw data\n- Refactor to enable history tracking of transactions in OLAP database\n";
-    expect(result["future features"]).toEqual(expected);
+      "- Improvements to integration testing\n- Restructure ingestion bucket data to better track raw data\n- Refactor to enable history tracking of transactions in OLAP database";
+    expect(result["future"]).toEqual(expected);
+  });
+  test("should return an object with the key 'readme' whose value is a string equal to the input parameter", () => {
+    const result = convertReadMe(input);
+    expect(result.readme).toEqual(input);
   });
 });
--- controller.js
+++ controller.js
@@ -6,46 +6,57 @@ const conn = axios.create({
   headers: { Accept: "application/vnd.github+json", "User-Agent": "runraga" },
 });
 
-module.exports.getPublicRepos = (req, res) => {
-  conn.get("/users/runraga/repos").then((githubRes) => {
-    res.status(200).send({ data: githubRes.data });
-  });
+getPublicRepos = (req, res, next) => {
+  conn
+    .get("/users/runraga/repos")
+    .then((githubRes) => {
+      res.header("Access-Control-Allow-Origin", "*");
+      console.log(githubRes.data);
+      res.status(200).send(githubRes.data);
+    })
+    .catch((err) => {
+      next(err);
+    });
 };
 
-module.exports.getReadmeContents = (req, res) => {
-  const { repo } = req.params;
-  conn.get(`/repos/runraga/${repo}/contents/README.md`).then((githubRes) => {
-    console.log(githubRes);
-    decoded_contents = atob(githubRes.data.content);
-
-    res.status(200).send({ data: decoded_contents });
-  });
-  //"https://api.github.com/repos/runraga/be-pets-and-owners-solo/contents/{+path}"
-};
-
-module.exports.convertReadMe = (decodedReadme) => {
+convertReadMe = (decodedReadme) => {
+  const regexLookup = {
+    title: "#",
+    description: "Short description",
+    languages: "Languages used",
+    overview: "Overview",
+    future: "Future Features",
+  };
   const readmeInfo = {};
-  const title = decodedReadme.match(/(?<=#) ([\w ]+)(?=\n)/);
-  if (title) {
-    readmeInfo.title = title[1];
+  for (const [k, v] of Object.entries(regexLookup)) {
+    const regex = new RegExp(`(?<=${v})[^#]+`);
+    const matches = decodedReadme.match(regex);
+    if (matches) {
+      readmeInfo[k] = matches[0].trim();
+    }
   }
-  const shortDescription = decodedReadme.match(/(?<=Short description\n)(.+)/);
-  if (shortDescription) {
-    readmeInfo["short description"] = shortDescription[1];
-  }
-  const languages = decodedReadme.match(/(?<=Languages used\n)(.+\n)+/);
-  if (languages) {
-    noNewLines = languages[0].replaceAll("\n", "");
-    langArr = noNewLines.split("- ");
-    readmeInfo.languages = langArr.slice(1);
-  }
-  const overview = decodedReadme.match(/(?<=Overview\n)[^#]+/);
-  if (overview) {
-    readmeInfo.overview = overview[0];
-  }
-  const future = decodedReadme.match(/(?<=Future Features\n)[^#]+/);
-  if (future) {
-    readmeInfo["future features"] = future[0];
-  }
+  readmeInfo.readme = decodedReadme;
   return readmeInfo;
+};
+
+getReadmeContents = (req, res, next) => {
+  const { repo } = req.params;
+  conn
+    .get(`/repos/runraga/${repo}/contents/README.md`)
+    .then((githubRes) => {
+      decoded_contents = atob(githubRes.data.content);
+      data = convertReadMe(decoded_contents);
+      res.header("Access-Control-Allow-Origin", "*");
+      res.status(200).send({ data });
+    })
+    .catch((err) => {
+      console.log(err);
+      next(err);
+    });
+};
+
+module.exports = {
+  getReadmeContents,
+  convertReadMe,
+  getPublicRepos,
 };
