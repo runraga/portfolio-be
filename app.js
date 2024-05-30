c2ca5601a899b813e114aee51d1caa984cc0926c
runraga <james@helmfarm.com>
2024-05-30 15:48:07 +0100

    add middleware for CORS

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require("express");
-const cors = require("cors")
+// const cors = require("cors")
 const { getPublicRepos, getReadmeContents } = require("./controller");
 require("dotenv").config();
 const app = express();
 app.use(express.json());
 
-app.use(cors());
+app.options("/portfolio-be", (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://example.com");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.sendStatus(204);
+});
 
 app.get("/portfolio-be", getPublicRepos);
 
