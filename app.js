6c338214974f84cf6e4d8539b29e717ae2e267d0
runraga <james@helmfarm.com>
2024-05-30 14:34:58 +0100

    correct app endpoint

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 const app = express();
 app.use(express.json());
 
-app.get("/portfolio-be/", getPublicRepos);
+app.get("/portfolio-be", getPublicRepos);
 
 // app.get("/portfolio-be/readme/:repo", getReadmeContents);
 
