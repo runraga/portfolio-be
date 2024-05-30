970c7e276c91d00c9df6db4f76528366f98eca35
runraga <james@helmfarm.com>
2024-05-30 14:40:10 +0100

    add CORS header exemption

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const { getPublicRepos, getReadmeContents } = require("./controller");
-require('dotenv').config()
+require("dotenv").config();
 const app = express();
 app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 
 app.get("/portfolio-be", getPublicRepos);
 
