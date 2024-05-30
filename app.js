3a40e9e9f19d6f8c1d0599d068e993c89cedaac1
runraga <james@helmfarm.com>
2024-05-30 14:48:00 +0100

    set cors origin in preflight request

--- app.js
+++ app.js
@@ -4,12 +4,15 @@ require("dotenv").config();
 const app = express();
 app.use(express.json());
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://portfolio.helmfarm.co.uk");
+app.options("/portfolio-be", (req, res) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://portfolio.helmfarm.co.uk"
+  );
   res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  next();
-})
+  res.sendStatus(204);
+});
 
 app.get("/portfolio-be", getPublicRepos);
 
