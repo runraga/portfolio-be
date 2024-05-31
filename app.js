6991754e1395097bda080360a2a003ce9f16e15c
runraga <james@helmfarm.com>
2024-05-31 12:03:08 +0100

    add headers to enable cors

--- app.js
+++ app.js
@@ -5,12 +5,7 @@ require("dotenv").config();
 const app = express();
 app.use(express.json());
 
-app.options("*", (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  res.sendStatus(204);
-});
+app.use(cors());
 
 app.get("/portfolio-be", getPublicRepos);
 
